865063455549a448f420a8b1d636435b7863e58a
mateuslll <lima.mt96@gmail.com>
2024-06-07 19:10:39 -0300

    import method post

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -16,4 +16,31 @@ public class ProductModel implements Serializable {
     private UUID id;
     private String name;
     private BigDecimal value;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
+
+
